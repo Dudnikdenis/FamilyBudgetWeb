f3a4691dfdee2dc9273d9feb9fc1584f14a02fb8
Denwee <dudnik.denis.87@mail.ru>
2023-10-28 01:25:51 +0500

    коммунальные платежи 3

--- backend/controller/communalPayments.controller.js
+++ backend/controller/communalPayments.controller.js
@@ -7,30 +7,59 @@ class communalPaymentsController {
     async GetcommunalPayments(req,res){
 
         const month = ["Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"];
-        const electricity_data = new Date();
+        const data = new Date();
         const electricity_rate = await db.query('select (rate) from communal_rate where id = 1;' );
         const water_rate = await db.query('select (rate) from communal_rate where id = 2;' );
-        const electricity = await db.query(`select * from electricity where date_indications = ${electricity_data.getMonth()};`);
-        const water = await db.query(`select * from water where date_indications = ${electricity_data.getMonth()};`);
+        const starting_indication_electricity = await db.query(`select * from electricity where date_indications = ${data.getMonth()-1};`);
+        const starting_indication_water = await db.query(`select * from water where date_indications = ${data.getMonth()-1};`);
+        const final_indications_electricity = await db.query(`select * from electricity where date_indications = (SELECT MAX(date_indications) FROM electricity);`);
+        const final_indications_water = await db.query(`select * from water where date_indications = (SELECT MAX(date_indications) FROM water);`);
+        
+        // убрать повторение. сделать дополнительный метод
+
+        let result;
+        if((final_indications_water.rows[0].date_indications===data.getMonth()+1)){
+            result ={
+                electricity: {
+                    starting_indication:starting_indication_electricity.rows[0].indications,
+                    date_starting_indication: month[starting_indication_electricity.rows[0].date_indications-1],
+                    final_indications:final_indications_electricity.rows[0].indications,
+                    rate: electricity_rate.rows[0].rate,
+                    amount:(final_indications_electricity.rows[0].indications-starting_indication_electricity.rows[0].indications)*electricity_rate.rows[0].rate
+                },
+                water:{
+                    starting_indication:starting_indication_water.rows[0].indications,
+                    date_starting_indication:  month[starting_indication_water.rows[0].date_indications-1],  
+                    final_indications:final_indications_water.rows[0].indications,
+                    rate: water_rate.rows[0].rate,
+                    amount:(final_indications_water.rows[0].indications-starting_indication_water.rows[0].indications)*water_rate.rows[0].rate
+                }
+    
+            };
+        }
+
+        else{
+            result ={
+                electricity: {
+                    starting_indication:starting_indication_electricity.rows[0].indications,
+                    date_starting_indication: month[starting_indication_electricity.rows[0].date_indications-1],// доделать подсчет суммы и вывод показаний текущего месяца если показания уже ввели
+                    final_indications:0,
+                    rate: electricity_rate.rows[0].rate,
+                    amount:0
+                },
+                water:{
+                    starting_indication:starting_indication_water.rows[0].indications,
+                    date_starting_indication:  month[starting_indication_water.rows[0].date_indications-1],  //тоже самое
+                    final_indications:0,
+                    rate: water_rate.rows[0].rate,
+                    amount:0
+                }
+    
+            };
+        }
 
         
-        const result ={
-            electricity: {
-                starting_indication:electricity.rows[0].indications,
-                date_starting_indication: month[electricity.rows[0].date_indications-1],// доделать подсчет суммы и вывод показаний текущего месяца если показания уже ввели
-                final_indications:0,
-                rate: electricity_rate.rows[0].rate,
-                amount:0
-            },
-            water:{
-                starting_indication:water.rows[0].indications,
-                date_starting_indication:  month[water.rows[0].date_indications-1],  //тоже самое
-                final_indications:0,
-                rate: water_rate.rows[0].rate,
-                amount:0
-            }
-
-        };
+        
         return res.json(result)        
     };
 
@@ -46,6 +75,8 @@ class communalPaymentsController {
             return res = "Показания уже есть.";
         }
     }; 
+
+
 }
 
 
